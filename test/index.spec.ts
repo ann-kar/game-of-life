d7f287a9f34b752f549d1add40dff6e7a9458caf
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-04-19 09:43:22 +0200

    first commit

---
+++ test/index.spec.ts
@@ -0,0 +1,11 @@
+import {add} from "../src/index";
+
+describe("test", () => {
+  it("test", () => {
+    expect(2 + 2).toBe(4);
+  });
+
+  it('test 2', () => {
+    expect(add(5,6)).toBe(11);
+  });
+});
